e9c6f8aafeace6cdf70772ffac87aca04f08ce8b
Gautam Jadhav <p.gautam.jadhav@accenture.com>
2019-12-18 17:25:21 +0530

    Added Collection Preview and Collection Item. Studied Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
+import Homepage from './pages/homepage/homepage.component';
+import Shop from './pages/shop/shop.component';
+import {Switch, Route} from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <div>
+    <Switch>
+    <Route exact path='/' component={Homepage}/>
+    <Route path='/shop' component={Shop}/>
+    </Switch>
+    </div>
+    
   );
 }
 
---
+++ src/pages/shop/shop.component.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import SHOP_DATA from './shop.data';
+
+import CollectionPreview from'../../components/collection-preview/collection-preview.component';
+
+class Shop extends React.Component{
+constructor(){
+	super();
+	this.state ={
+		collection:SHOP_DATA,
+	}
+}
+
+render(){
+	const {collection} = this.state;
+	return(
+		<div className="shop-page">
+		{
+			collection.map(({id,...otherProps})=>
+				(<CollectionPreview key={id} {...otherProps} />)
+			)
+		}
+		</div>
+		)
+}
+}
+
+export default Shop;
