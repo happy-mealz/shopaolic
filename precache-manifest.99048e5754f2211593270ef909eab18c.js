fcf9a9d9197060b9b3c7a5c02b8703c8efe07fef
Gautam Jadhav <p.gautam.jadhav@accenture.com>
2019-12-23 17:50:49 +0530

    Updates

---
+++ precache-manifest.99048e5754f2211593270ef909eab18c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8522365048b9062fd56100cf5be20efd",
+    "url": "/shopaolic/index.html"
+  },
+  {
+    "revision": "64072ed386514d84ae86",
+    "url": "/shopaolic/static/css/main.13f5b6bb.chunk.css"
+  },
+  {
+    "revision": "7fd78a1b2fb4f157bd5c",
+    "url": "/shopaolic/static/js/2.1e1256fe.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/shopaolic/static/js/2.1e1256fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "64072ed386514d84ae86",
+    "url": "/shopaolic/static/js/main.148b141a.chunk.js"
+  },
+  {
+    "revision": "85f1091e3e8daf59e930",
+    "url": "/shopaolic/static/js/runtime-main.a440265d.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/shopaolic/static/media/crown.ed28accb.svg"
+  }
+]);
